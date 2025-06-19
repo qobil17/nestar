8e32814f0d2225a99865eb20045d256c9365352d
qobil17 <ibrohimovqobilbek5@gmail.com>
2025-06-19 16:09:37 +0900

    fix: updateMemberByAdmin

--- apps/nestar-api/src/components/member/member.resolver.ts
+++ apps/nestar-api/src/components/member/member.resolver.ts
@@ -87,9 +87,9 @@ export class MemberResolver {
 	@Roles(MemberType.ADMIN)
 	@UseGuards(RolesGuard)
 	@Mutation(() => Member)
-	public async updateMembersByAdmin(@Args('input') input: MemberUpdate): Promise<Member> {
-		console.log('Mutation: updateMembersByAdmin');
-		return await this.memberService.updateMembersByAdmin(input);
+	public async updateMemberByAdmin(@Args('input') input: MemberUpdate): Promise<Member> {
+		console.log('Mutation: updateMemberByAdmin');
+		return await this.memberService.updateMemberByAdmin(input);
 	}
 
 	/** UPLOADER **/

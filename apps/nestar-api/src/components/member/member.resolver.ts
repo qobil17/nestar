816450cdb0b92b3f61083649ffe5085c412c0d86
qobil17 <ibrohimovqobilbek5@gmail.com>
2025-06-25 12:58:27 +0900

    feat: develop likeTargetMember GraphQL api

--- apps/nestar-api/src/components/member/member.resolver.ts
+++ apps/nestar-api/src/components/member/member.resolver.ts
@@ -74,6 +74,16 @@ export class MemberResolver {
 		return await this.memberService.getAgents(memberId, input);
 	}
 
+	@UseGuards(AuthGuard)
+	@Mutation(() => Member)
+	public async LikeTargetMember(
+		@Args('memberId') input: string,
+		@AuthMember('_id') memberId: ObjectId,
+	): Promise<Member> {
+		console.log('Mutation: LikeTargetMember');
+		const LikeRefId = shapeIntoMongoObjectId(input);
+		return await this.memberService.LikeTargetMember(memberId, LikeRefId);
+	}
 	/** ADMIN **/
 
 	@Roles(MemberType.ADMIN)

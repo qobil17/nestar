7a1ad967c411ec8bb1eef950fcd805734c7467d7
qobil17 <ibrohimovqobilbek5@gmail.com>
2025-07-01 13:16:30 +0900

    feat: create batchRollback, batchProperties and batchAgents jobSchedulers logic

--- apps/nestar-batch/src/batch.controller.ts
+++ apps/nestar-batch/src/batch.controller.ts
@@ -1,12 +1,44 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Logger } from '@nestjs/common';
 import { BatchService } from './batch.service';
+import { Cron, Interval, Timeout } from '@nestjs/schedule';
+import { BATCH_ROLLBACK, BATCH_TOP_AGENTS, BATCH_TOP_PROPERTIES } from './lib/config';
 
 @Controller()
 export class BatchController {
-	constructor(private readonly nestarBatchService: BatchService) {}
+	private logger: Logger = new Logger('BatchController');
+
+	constructor(private readonly batchService: BatchService) {}
+
+	@Timeout(1000)
+	handleTimeout() {
+		this.logger.debug('BATCH SERVER READY');
+	}
+
+	@Cron('00 * * * * *', { name: BATCH_ROLLBACK })
+	public batchRollback() {
+		this.logger['context'] = BATCH_ROLLBACK;
+		this.logger.debug('EXECUTED!');
+	}
+
+	@Cron('20 * * * * *', { name: BATCH_TOP_PROPERTIES })
+	public batchProperties() {
+		this.logger['context'] = BATCH_TOP_PROPERTIES;
+		this.logger.debug('EXECUTED!');
+	}
+
+	@Cron('40 * * * * *', { name: BATCH_TOP_AGENTS })
+	public batchAgents() {
+		this.logger['context'] = BATCH_TOP_AGENTS;
+		this.logger.debug('EXECUTED!');
+	}
+
+	/* 	@Interval(1000)
+	handleInterval() {
+		this.logger.debug('INTERNAL TEST');
+  }*/
 
 	@Get()
 	getHello(): string {
-		return this.nestarBatchService.getHello();
+		return this.batchService.getHello();
 	}
 }
--- apps/nestar-batch/src/batch.service.ts
+++ apps/nestar-batch/src/batch.service.ts
@@ -5,4 +5,16 @@ export class BatchService {
 	getHello(): string {
 		return 'Welcome to Nestar BATCH Serrver!';
 	}
+
+	public async batchRollback(): Promise<void> {
+		console.log('batchRollback');
+	}
+
+	public async batchProperties(): Promise<void> {
+		console.log('batchProperties');
+	}
+
+	public async batchAgents(): Promise<void> {
+		console.log('batchAgents');
+	}
 }
